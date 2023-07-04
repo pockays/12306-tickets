516353a17bd4d720bd14847bcaf3c7f9fa19bdab
liuris <1159419473@qq.com>
2023-07-04 16:09:52 +0800

    写的差不多了，细节之后处理

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # 导入selenium模块
 import openpyxl
 from selenium import webdriver
+from selenium.common import NoSuchElementException, ElementNotVisibleException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
@@ -14,13 +16,18 @@ class TrainSpider(object):
     login_url = 'https://kyfw.12306.cn/otn/resources/login.html'  # 登录的页面
     profile_url = 'https://kyfw.12306.cn/otn/view/index.html'  # 个人中心的网址
     left_ticket = 'https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc'  # 余票查询
+    confirm_url = 'https://kyfw.12306.cn/otn/confirmPassenger/initDc'  # 确认界面
 
-    def __init__(self, from_station, to_station, train_date, trains):
+    def __init__(self, from_station, to_station, train_date, trains, passengers, ticket_type):
+        self.selected_no = None
         self.from_station = from_station
         self.to_station = to_station
         self.train_date = train_date
         self.station_code = self.init_station_code()
         self.trains = trains
+        self.passengers = passengers
+        self.ticket_type = ticket_type
+        self.seat_type = None
 
     def login(self):
         # 打开12306的网页
@@ -75,44 +82,92 @@ class TrainSpider(object):
         WebDriverWait(driver, 1000).until(
             ec.presence_of_element_located((By.XPATH, '//tbody[@id = "queryLeftTable"]/tr'))
         )
-        trains = driver.find_elements(By.XPATH, '//tbody[@id = "queryLeftTable"]/tr[not(@datatran)])')
+        trains = driver.find_elements(By.XPATH, '//tbody[@id = "queryLeftTable"]/tr[not(@datatran)]')
         is_flag = False
-        for train in trains:
-            infos = train.text.split('\n')
-            train_no = infos[0] # 显示车次
-            if train_no in self.trains:    # 是否存在需求车次
-                seat_types = self.trains[train_no]
-                for seat_type in seat_types:  # 查找是否有位置
-                    if seat_type == 'o':  # 如果是二等座
-                        count = infos[9]
-                        if count.isdigit() or count == '有':
-                            is_flag = True
-                            break  # 退出席位循环
-                    elif seat_type == 'M':
-                        count = infos[8]
-                        if count.isdigit() or count == '有':
-                            is_flag = True
-                            break
-                # 是否有余票
-                if is_flag:
-                    order_btn = train.find_element(By.XPATH, './/a[@class="btn72"]')  # 注意是.// 表示当前路径，否则是在全局选择，而不是当前行
-                    order_btn.click()
-                    break  # 退出车次循环
+        while True:
+            for train in trains:
+                infos = train.text.replace('/n', ' ').split(' ')
+                train_no = infos[0]  # 显示车次
+                if train_no in self.trains:  # 是否存在需求车次
+                    seat_types = self.trains[train_no]
+                    for seat_type in seat_types:  # 查找是否有位置
+                        if seat_type == 'O':  # 如果是二等座
+                            count = infos[9]
+                            if count.isdigit() or count == '有':
+                                is_flag = True
+                                break  # 退出席位循环
+                        elif seat_type == 'M':
+                            count = infos[8]
+                            if count.isdigit() or count == '有':
+                                is_flag = True
+                                break
+                    # 是否有余票
+                    if is_flag:
+                        order_btn = train.find_element(By.XPATH,
+                                                       './/a[@class="btn72"]')  # 注意是.// 表示当前路径，否则是在全局选择，而不是当前行
+                        order_btn.click()
+                        self.selected_no = train_no
+                        return  # 退出该函数
+                    else:
+                        print('所选座位没票了')
+                        return
 
 
+    def confirm(self):
+        WebDriverWait(driver, 1000).until(
+            ec.url_to_be(self.confirm_url)
+        )
+        WebDriverWait(driver, 1000).until(
+            ec.presence_of_element_located((By.XPATH, '//ul[@id = "normal_passenger_id"]/li/label'))
+        )
+        passengers = driver.find_elements(By.XPATH, '//ul[@id = "normal_passenger_id"]/li/label')
+        for passenger in passengers:
+            passenger_name = passenger.text.replace('（学生）', '')  # 学生认证后会有（学生）
+            if passenger_name in self.passengers:
+                passenger.click()
+        # 选择票类
+        ticket_select = Select(driver.find_element(By.ID, 'ticketType_1'))
+        ticket_select.select_by_value(self.ticket_type)
+        # 选择席位
+        seat_select = Select(driver.find_element(By.ID, 'seatType_1'))
+        seat_types = self.trains[self.selected_no]
+        for seat_type in seat_types:
+            try:
+                seat_select.select_by_value(seat_type)
+                self.seat_type = seat_type
+            except NoSuchElementException:
+                continue
+            else:
+                break
+        # 提交订单
+        WebDriverWait(driver, 1000).until(
+            ec.element_to_be_clickable((By.ID, 'submitOrder_id'))
+        )
+        submit_btn = driver.find_element(By.ID, 'submitOrder_id')
+        submit_btn.click()
 
-
-
-
-
-
-
+        WebDriverWait(driver, 1000).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'dhtmlx_window_active'))
+        )
+        WebDriverWait(driver, 1000).until(
+            ec.element_to_be_clickable((By.ID, 'qr_submit_id'))
+        )
+        submit_btn = driver.find_element(By.ID, 'qr_submit_id')
+        while submit_btn:
+            try:
+                submit_btn.click()
+                submit_btn = driver.find_element(By.ID, 'qr_submit_id')
+            except ElementNotVisibleException:
+                break
+        print(f'恭喜{self.selected_no}的{self.seat_type}抢票成功！')
 
     def run(self):
         # 登录
         self.login()
         # 余票查询
         self.search_ticket()
+        # 确认订单
+        self.confirm()
 
     def init_station_code(self):
         wb = openpyxl.load_workbook('车站代码.xlsx')
@@ -125,8 +180,9 @@ class TrainSpider(object):
             lst.append(sub_lst)
         return dict(lst)
 
-    # def start():                                         o为二等座，m为一等座，9为商务座
-    spider = TrainSpider('葫芦岛', '大连', '2023-07-16', {'D30': ['o', 'M']})
+
+def start():  # o为二等座，m为一等座，9为商务座         票类别分为 成人票为1 儿童票为2 学生票为3 残军票为4
+    spider = TrainSpider('葫芦岛', '大连', '2023-07-16', {'D8087': ['O', 'M']}, ['郭政熠'], '1')
     spider.run()
 
 
